6f5752e5e091a13fde78d56727908c57a31aaf94
sirkinshu <shreyanshsawarn88@gmail.com>
2020-05-08 21:45:06 +0530

    LMS

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://ss.com/" target="_blank"  > Designed by : Shreyansh Sawarn</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
